95d1d36eac7c879852ec48a1a45fea7ec37128d5
Justin Simon <development@jsimon.me>
2019-11-19 20:35:48 -0500

    Updated roles and playbook

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ fi
 
 if ! hash ansible >/dev/null 2>&1; then
     echo "Installing Ansible..."
-    sudo apt-get update
+    sudo apt-get update 
+    sudo apt-get upgrade
     sudo apt-get install software-properties-common ansible git python-apt -y
 else
     echo "Ansible already installed"
